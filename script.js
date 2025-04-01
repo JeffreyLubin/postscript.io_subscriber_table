bd5b0c7b11c2118185fad74a68e82ce8eab02335
Jeffrey Lubin <jeffy.lubin@gmail.com>
2025-03-31 22:51:14 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+let currentPage = 1;
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: "Bearer sk_6cd914fbb2c682a0626e85de06faed40"
+    }
+  };
+
+function getSubscriberPage(page) { 
+    fetch(`https://api.postscript.io/api/v2/subscribers?page=${page}`, options)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            subscriberTable(data.subscribers);
+        })
+        .catch(err => console.error(err));
+}
+
+function subscriberTable(subscribers){
+
+        const tableBody = document.getElementById("subscriber_table")
+        tableBody.innerHTML = ''
+        
+        subscribers.forEach(subscriber => {
+            const row = document.createElement("tr")
+            row.innerHTML = 
+            `
+            <td>${subscriber.phone_number || "N/A"}</td>
+            <td>${subscriber.email_address || "N/A"}</td>
+            <td>${subscriber.created_at || "N/A"}</td>
+            <td>${subscriber.tags || "N/A"}</td>
+            <td><input id="tagInput_${subscriber.id}" type="text" placeholder="Enter new tag..."></td>
+            <td><button onclick="updateTag('${subscriber.id}')" type="button">Submit New Tag!</button></td>
+            `
+            tableBody.append(row);
+        });
+
+        document.getElementById("prevPage").disabled = currentPage === 1;
+}
+
+function updateTag(subscriber_id){
+    const tagInput = document.getElementById(`tagInput_${subscriber_id}`);
+    console.log(tagInput)
+    const newTag = tagInput.value;
+
+    if (!newTag){
+        alert("Please enter a valid field.")
+        return;
+    }
+
+    const options = {
+        method: 'PATCH',
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json',
+          Authorization: "Bearer sk_6cd914fbb2c682a0626e85de06faed40"
+        },
+        body: JSON.stringify({tags: [newTag]})
+      };
+      
+      fetch(`https://api.postscript.io/api/v2/subscribers/${subscriber_id}`, options)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            location.reload();
+        })
+        .catch(err => console.error(err));
+}
+
+document.getElementById("nextPage").addEventListener('click', function(){
+    currentPage++
+    getSubscriberPage(currentPage)
+})
+
+document.getElementById("prevPage").addEventListener('click', function(){
+    if (currentPage > 1){
+        currentPage--
+        getSubscriberPage(currentPage)
+    }
+})
+
+getSubscriberPage(currentPage)
