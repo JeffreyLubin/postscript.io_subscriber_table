d1c1c78cebb92b9b6dc554cdacbcf4c5dabeb8c9
Jeffrey Lubin <jeffy.lubin@gmail.com>
2025-03-31 23:49:17 -0500

    Revert

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: `Bearer ${process.env.API_KEY}`
+      Authorization: `Bearer sk_6cd914fbb2c682a0626e85de06faed40`
     }
   };
 
@@ -55,7 +55,7 @@ function updateTag(subscriber_id){
         headers: {
           accept: 'application/json',
           'content-type': 'application/json',
-          Authorization: `Bearer ${process.env.API_KEY}`
+          Authorization: `Bearer sk_6cd914fbb2c682a0626e85de06faed40`
         },
         body: JSON.stringify({tags: [newTag]})
       };
